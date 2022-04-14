3103377ddc4595e3b687158cee4c3eb4fa84dc8b
bovle <frederik@live.dk>
2022-04-14 21:07:52 +0200

    added template

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from test import run_benchmark, run_table
-from poker_game_runner.bots import foldBot, randombot
+from poker_game_runner.bots import randombot
 from example_bots import panic_bot, odds_bot, checkmate, position_bot
+import my_bot
 
-bots = [panic_bot, odds_bot, checkmate, position_bot, randombot, panic_bot, odds_bot, checkmate, position_bot,randombot]
+bots = [panic_bot, odds_bot, checkmate, position_bot, randombot, panic_bot, odds_bot, checkmate, position_bot, my_bot]
 
 #run_table(bots)
 
-json = run_benchmark(bots, 30)
+run_benchmark(bots, 30)
  
